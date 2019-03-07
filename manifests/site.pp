564187f7e301b6a23e43d3c5b7c8e2441c131c5d
Axel Haenssen <axel@princeton.edu>
2019-03-07 14:33:46 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'this is a readme too',
-    owner   => 'root',
-  }
+}
+node 'localhost' {
+  include role::master_server
 }
